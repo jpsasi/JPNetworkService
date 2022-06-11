4c7c7faa362bee0208718a022eaed4748a7d7b78
Sasikumar JP <jpsasi@gmail.com>
2022-06-11 18:16:02 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# JPNetworkService
+
+A description of this package.
---
+++ Tests/JPNetworkServiceTests/JPNetworkServiceTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import JPNetworkService
+
+final class JPNetworkServiceTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(JPNetworkService().text, "Hello, World!")
+    }
+}
